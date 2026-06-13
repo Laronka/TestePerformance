6ebf8bc8830905912551850175f661eb6fc624d4
joaoiaronka622@gmail.com <joaoiaronka622@gmail.com>
2026-06-13 11:02:34 -0300

    criado host estático no código e função que simula busca de um produto

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,13 @@
 from locust import HttpUser, task, between
 
 class JuiceShopUser(HttpUser):
+    host = "http://juice-shop:3000"
     wait_time = between(1, 3)
 
     @task
     def homepage(self):
         self.client.get("/")
+
+    @task
+    def busca(self):
+        self.client.get("/rest/products/search?q=apple")
