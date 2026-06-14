fb5ba5d08f3357fab881ed2e627fd5d7ca4b88a4
joaoiaronka622@gmail.com <joaoiaronka622@gmail.com>
2026-06-14 18:49:40 -0300

    Configurado func on_start para criar contas de testes aleatória e random no carrinho

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,11 @@
+import random
 from locust import HttpUser, task, between
+
+produtos_id = [
+    1,2,3,4,5,6,7,8,9,13,14,15,16,17,18,19,20,
+    21,22,23,24,25,26,29,30,32,33,34,35,36,37,38,
+    41,42,43,45,47,48,49,50,51,52,53,54,55,56
+    ]
 
 class JuiceShopUser(HttpUser):
     host = "http://juice-shop:3000"
@@ -8,9 +15,11 @@ class JuiceShopUser(HttpUser):
     basket_id = None
 
     def on_start(self):
-        
+        self.produtos_no_carrinho = set()
+        self.email = f"user_{id(self)}@juice.teste"
+
         self.client.post("/api/Users", json={
-            "email": "joaoiaronka622@gmail.com",
+            "email": self.email,
             "password": "Elo2026",
             "passwordRepeat": "Elo2026",
             "securityQuestion": {
@@ -21,7 +30,7 @@ class JuiceShopUser(HttpUser):
         })
 
         resp = self.client.post("/rest/user/login", json={
-            "email": "joaoiaronka622@gmail.com",
+            "email": self.email,
             "password": "Elo2026"
         })
         if resp.status_code == 200:
@@ -44,13 +53,27 @@ class JuiceShopUser(HttpUser):
     @task
     def login(self):
         self.client.post("/rest/user/login", json={
-        "email": "joaoiaronka622@gmail.com",
+        "email": self.email,
         "password": "Elo2026"
     })
+        
+
     @task
     def carrinho(self):
-         self.client.post("/api/BasketItems", json={
-        "ProductId": 1,
-        "BasketId": 1,
-        "quantity": 1
-    })
+        if not self.token or not self.basket_id:
+            return
+    
+        disponiveis = [i for i in produtos_id if i not in self.produtos_no_carrinho]
+        if not disponiveis:
+            return  
+        produto = random.choice(disponiveis)
+        resp = self.client.post("/api/BasketItems",
+        json={
+            "ProductId": produto,
+            "BasketId": self.basket_id,
+            "quantity": 1
+        },
+        headers={"Authorization": f"Bearer {self.token}"}
+    )
+        if resp.status_code in (200, 201):
+            self.produtos_no_carrinho.add(produto)
